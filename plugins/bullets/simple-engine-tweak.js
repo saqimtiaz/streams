0b8929d10da6054874cc08b92bc40afb279d5fb7
saqimtiaz <saq.imtiaz@gmail.com>
2020-05-26 20:15:10 +0200

    first prototype from local repo

---
+++ plugins/bullets/simple-engine-tweak.js
@@ -0,0 +1,12 @@
+var SimpleEngine = require("$:/core/modules/editor/engines/simple.js").SimpleEngine;
+
+SimpleEngine.prototype.focus = function() {
+	if(this.domNode.focus && this.domNode.select) {
+		this.domNode.focus();
+		if(this.widget.getAttribute("select")==="false") {
+			this.domNode.setSelectionRange(this.domNode.value.length,this.domNode.value.length);
+		} else {
+			this.domNode.select();
+		}
+	}
+}
