9fc4d978198874bc69a3fcf792790af82d73c64c
saqimtiaz <saq.imtiaz@gmail.com>
2020-05-28 01:31:47 +0200

    Added first version of an extensible fitler based hook mechanism for deleting tiddlers. Currently only removes the deleted tiddler from its parent's sq-bullets field

---
+++ tiddlers/plugins/sq/bullets/delete-hook/hook.js
@@ -0,0 +1,25 @@
+$tw.hooks.addHook("th-deleting-tiddler",function(tiddler) {
+	if(tiddler.fields["draft.of"]) {
+		return;
+	}
+	
+	var executeDeleteHookTiddlers = function(tag) {
+		$tw.utils.each($tw.wiki.filterTiddlers("[all[shadows+tiddlers]tag[" + tag + "]!has[draft.of]]"),function(title) {
+			var parser = $tw.wiki.parseText("text/vnd.tiddlywiki",$tw.wiki.getTiddlerText(title),{
+					parentWidget: $tw.rootWidget,
+					document: this.document
+				}),
+				widgetNode = $tw.wiki.makeWidget(parser,{
+					parentWidget: $tw.rootWidget,
+					document: this.document,
+					variables: {currentTiddler : tiddler.fields.title}
+				});
+			var container = this.document.createElement("div");
+			widgetNode.render(container,null);
+			widgetNode.invokeActions(this,null);	
+		});
+	}
+	
+	executeDeleteHookTiddlers("$:/tags/deleteTiddlerAction");
+
+});
