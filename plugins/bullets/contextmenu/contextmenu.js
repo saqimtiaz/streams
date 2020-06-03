8f5c47f568cdc60f770ae00ee5a2c2566aec5509
saqimtiaz <saq.imtiaz@gmail.com>
2020-06-03 08:15:09 +0200

    Added exploratory attempt at a context menu, triggered by right clicking the bullet symbol

---
+++ plugins/bullets/contextmenu/contextmenu.js
@@ -0,0 +1,63 @@
+/*\
+
+title: $:/plugins/sq/bullets/contextmenu/contextmenu
+type: application/javascript
+module-type: widget-subclass
+
+\*/
+(function(){
+	
+exports.baseClass = "button";
+
+exports.name = "contextmenu";
+	
+exports.constructor = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+exports.prototype = {};	
+
+exports.prototype.render = function(parent,nextSibling) {
+	var self = this;
+	// Remember parent
+	this.parentDomNode = parent;
+	// Compute attributes and execute state
+	this.computeAttributes();
+	this.execute();
+	// Create element
+	var tag = "div";
+	if(this.buttonTag && $tw.config.htmlUnsafeElements.indexOf(this.buttonTag) === -1) {
+		tag = this.buttonTag;
+	}
+	var domNode = this.document.createElement(tag);
+	var classes = this["class"].split(" ") || [];
+	var isPoppedUp = (this.popup || this.popupTitle) && this.isPoppedUp();
+	if(this.selectedClass) {
+		if(isPoppedUp) {
+			$tw.utils.pushTop(classes,this.selectedClass.split(" "));
+		}
+	}
+	if(isPoppedUp) {
+		$tw.utils.pushTop(classes,"tc-popup-handle");
+	}	
+	classes.push("sq-contextmenu-container");
+	domNode.className = classes.join(" ");	
+	domNode.addEventListener("contextmenu",function(event) {
+		event.preventDefault();
+		if(self.popup || self.popupTitle) {
+			self.triggerPopup(event);
+			if(self.set || self.setTitle) {
+				self.setTiddler();
+				handled = true;
+			}
+			if(self.actions) {
+				self.invokeActionString(self.actions,self,event);
+			}
+		}
+	});
+	parent.insertBefore(domNode,nextSibling);
+	this.renderChildren(domNode,null);
+	this.domNodes.push(domNode);	
+}	
+	
+})();
