88539b391b32bf47e2ee1698f5a1b5e558975e79
saqimtiaz <saq.imtiaz@gmail.com>
2020-06-06 11:22:50 +0200

    Exploratory change to switch to dblclick as trigger for editing

---
+++ plugins/bullets/btn-dblclick.js
@@ -0,0 +1,56 @@
+/*\
+
+title: $:/plugins/sq/bullets/btn-dblclick
+type: application/javascript
+module-type: widget-subclass
+
+\*/
+(function(){
+	
+exports.baseClass = "button";
+
+exports.name = "btn-dblclick";
+	
+exports.constructor = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+exports.prototype = {};	
+
+exports.prototype.render = function(parent,nextSibling) {
+	var self = this;
+	// Remember parent
+	this.parentDomNode = parent;
+	// Compute attributes and execute state
+	this.computeAttributes();
+	this.execute();
+	// Create element
+	var tag = "div";
+	if(this.buttonTag && $tw.config.htmlUnsafeElements.indexOf(this.buttonTag) === -1) {
+		tag = this.buttonTag;
+	}
+	var domNode = this.document.createElement(tag);
+	var classes = this["class"].split(" ") || [];
+	domNode.className = classes.join(" ");	
+	domNode.addEventListener("dblclick",function(event) {
+		event.preventDefault();
+		if (window.getSelection) {
+			window.getSelection().removeAllRanges();
+		} else if (document.selection) {
+			document.selection.empty();
+		}
+		if(self.set || self.setTitle) {
+			self.setTiddler();
+		}
+		if(self.actions) {
+			self.invokeActionString(self.actions,self,event);
+		}
+		var x = self.invokeActions(self,event);
+		return true;
+	},false);
+	parent.insertBefore(domNode,nextSibling);
+	this.renderChildren(domNode,null);
+	this.domNodes.push(domNode);	
+}	
+	
+})();
