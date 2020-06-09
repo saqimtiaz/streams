4e36028a603105217f139e081dd2b9207d72f190
saqimtiaz <saq.imtiaz@gmail.com>
2020-06-09 10:34:56 +0200

    rename editor class

--- plugins/streams/widgets/action-withselection.js
+++ plugins/streams/widgets/action-withselection.js
@@ -74,7 +74,7 @@ Invoke the action associated with this widget
 */
 WithSelectionWidget.prototype.invokeAction = function(triggeringWidget,event) {
 	var selection;
-	var iframe = triggeringWidget.domNodes[0].querySelector("iframe.sq-bullet-editor.tc-edit-texteditor.tc-edit-texteditor-body");
+	var iframe = triggeringWidget.domNodes[0].querySelector("iframe.streams-editor.tc-edit-texteditor.tc-edit-texteditor-body");
 	if(iframe) {
 		var idoc= iframe.contentDocument || iframe.contentWindow.document;
 		selection = idoc.getSelection();
