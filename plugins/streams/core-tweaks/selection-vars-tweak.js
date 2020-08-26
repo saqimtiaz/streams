ba102be23249180e71a8af4df85a05d9a0c747cc
saqimtiaz <saq.imtiaz@gmail.com>
2020-08-26 15:31:20 +0200

    Tweaked framed engine and actions-macros to support placing caret at a specified position

--- plugins/streams/core-tweaks/selection-vars-tweak.js
+++ plugins/streams/core-tweaks/selection-vars-tweak.js
@@ -4,9 +4,6 @@ title: $:/plugins/sq/streams/selection-vars-tweak.js
 type: application/javascript
 module-type: startup
 \*/
-
-
-
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 Widget.prototype.selection_original_invokeActionString = Widget.prototype.invokeActionString;
@@ -37,3 +34,19 @@ Widget.prototype.invokeActionString = function(actions,triggeringWidget,event,va
 	this.selection_original_invokeActionString(actions,triggeringWidget,event,variables);
 }
 
+$tw.hooks.addHook("th-page-refreshed",function handleEvent() {
+	$tw.wiki.deleteTiddler("$:/state/sq/streams/caret-position");
+});
+
+/*
+
+if(window.CodeMirror && document.activeElement.closest(".CodeMirror")) {
+	var cm = document.activeElement.closest(".CodeMirror").CodeMirror;
+	var cursor = cm.getCursor("start");
+	var startRange = cm.getRange({"line":0,"ch":0},{"line":cursor.line,"ch":cursor.ch});
+	var selectionStart = startRange.length;
+	var selection = cm.getSelection();
+	var selectionEnd = selectionStart + selection.length();
+}
+
+*/
