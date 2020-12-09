8e76882bb64c25ae008673e9f8f417d12b55d48a
saqimtiaz <saq.imtiaz@gmail.com>
2020-12-09 22:16:57 +0100

    Propagate dblclick events from iframe editor to parent doc

--- plugins/streams/core-tweaks/framed-engine-tweak.js
+++ plugins/streams/core-tweaks/framed-engine-tweak.js
@@ -19,4 +19,22 @@ FramedEngine.prototype.focus = function() {
 			this.domNode.select();
 		}
 	}
+	if(!this.setupDblClick) {
+		this.domNode.addEventListener("dblclick",this.propagateDblClick.bind(this));
+		this.setupDblClick = true;
+	}
+}
+
+FramedEngine.prototype.propagateDblClick = function(event) {
+	var newEvent = this.widget.document.createEventObject ? this.widget.document.createEventObject() : this.widget.document.createEvent("Events");
+	if(newEvent.initEvent) {
+		newEvent.initEvent("dblclick", true, true);
+	}
+	newEvent.keyCode = event.keyCode;
+	newEvent.which = event.which;
+	newEvent.metaKey = event.metaKey;
+	newEvent.ctrlKey = event.ctrlKey;
+	newEvent.altKey = event.altKey;
+	newEvent.shiftKey = event.shiftKey;
+	return !this.widget.parentDomNode.dispatchEvent(newEvent);	
 }
