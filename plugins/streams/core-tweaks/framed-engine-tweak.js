87f4c32c08a2a060cdae403316c9414ac248998c
saqimtiaz <saq.imtiaz@gmail.com>
2021-01-04 22:02:49 +0100

    Added config option to double click in editor to exit editing

--- plugins/streams/core-tweaks/framed-engine-tweak.js
+++ plugins/streams/core-tweaks/framed-engine-tweak.js
@@ -26,15 +26,18 @@ FramedEngine.prototype.focus = function() {
 }
 
 FramedEngine.prototype.propagateDblClick = function(event) {
-	var newEvent = this.widget.document.createEventObject ? this.widget.document.createEventObject() : this.widget.document.createEvent("Events");
-	if(newEvent.initEvent) {
-		newEvent.initEvent("dblclick", true, true);
+	if(this.widget.wiki.getTiddlerText("$:/config/sq/streams/dblclick-editor-to-exit") === "yes") {
+		var newEvent = this.widget.document.createEventObject ? this.widget.document.createEventObject() : this.widget.document.createEvent("Events");
+		if(newEvent.initEvent) {
+			newEvent.initEvent("dblclick", true, true);
+		}
+		newEvent.keyCode = event.keyCode;
+		newEvent.which = event.which;
+		newEvent.metaKey = event.metaKey;
+		newEvent.ctrlKey = event.ctrlKey;
+		newEvent.altKey = event.altKey;
+		newEvent.shiftKey = event.shiftKey;
+		return !this.widget.parentDomNode.dispatchEvent(newEvent);
 	}
-	newEvent.keyCode = event.keyCode;
-	newEvent.which = event.which;
-	newEvent.metaKey = event.metaKey;
-	newEvent.ctrlKey = event.ctrlKey;
-	newEvent.altKey = event.altKey;
-	newEvent.shiftKey = event.shiftKey;
-	return !this.widget.parentDomNode.dispatchEvent(newEvent);	
+	return false;
 }
