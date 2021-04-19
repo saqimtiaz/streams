a7816f9f1459c5c7ab039aee1786f37eca028653
saqimtiaz <saq.imtiaz@gmail.com>
2021-04-19 22:27:39 +0200

    Do not use codemirror editor even if config specifies it if the widget is not available

--- plugins/streams/core-tweaks/codemirror-tweak.js
+++ plugins/streams/core-tweaks/codemirror-tweak.js
@@ -6,12 +6,19 @@ module-type: startup
 
 \*/
 
-if($tw.wiki.getTiddler("$:/plugins/tiddlywiki/codemirror")) {
-	var CMEngine = require("$:/plugins/tiddlywiki/codemirror/engine.js").CodeMirrorEngine;
-	CMEngine.prototype.focus = function() {
-		this.cm.focus();
-		var caretPosition = this.widget.getAttribute("caretPosition","end");	
-		var index = (caretPosition === "end") ? this.cm.getValue().length : parseInt(caretPosition);
-		this.cm.doc.setSelection(this.cm.doc.posFromIndex(index));
+exports.platforms = ["browser"];
+exports.synchronous = true;
+
+exports.startup = function() {
+
+	if($tw.wiki.getTiddler("$:/plugins/tiddlywiki/codemirror") && ($tw.wiki.getTiddlerText("$:/config/Plugins/Disabled/$:/plugins/tiddlywiki/codemirror","no") === "no")) {
+		var CMEngine = require("$:/plugins/tiddlywiki/codemirror/engine.js").CodeMirrorEngine;
+		CMEngine.prototype.focus = function() {
+			this.cm.focus();
+			var caretPosition = this.widget.getAttribute("caretPosition","end");	
+			var index = (caretPosition === "end") ? this.cm.getValue().length : parseInt(caretPosition);
+			this.cm.doc.setSelection(this.cm.doc.posFromIndex(index));
+		}
 	}
+
 }
--- plugins/streams/widgets/streams-edit.js
+++ plugins/streams/widgets/streams-edit.js
@@ -17,10 +17,15 @@ exports.prototype = {};
 exports.prototype.getEditorType = function() {
 	var tiddler = this.wiki.getTiddler(this.editTitle);
 	var type = tiddler.fields.type || "text/vnd.tiddlywiki";
+	var editorType;
 	if(type === "text/vnd.tiddlywiki") {
-		return this.wiki.getTiddlerText("$:/config/sq/streams/editor-engine");
+		editorType = this.wiki.getTiddlerText("$:/config/sq/streams/editor-engine");
+		if((!$tw.wiki.getTiddler("$:/plugins/tiddlywiki/codemirror") || $tw.wiki.getTiddlerText("$:/config/Plugins/Disabled/$:/plugins/tiddlywiki/codemirror","no") === "yes" || !$tw.modules.titles["$:/plugins/tiddlywiki/codemirror/edit-codemirror.js"]) && (editorType === "codemirror") ) {
+			editorType = "text";
+		}
+		return editorType;
 	}
-	var editorType = this.wiki.getTiddlerText(EDITOR_MAPPING_PREFIX + type);
+	editorType = this.wiki.getTiddlerText(EDITOR_MAPPING_PREFIX + type);
 	if(!editorType) {
 		var typeInfo = $tw.config.contentTypeInfo[type];
 		if(typeInfo && typeInfo.encoding === "base64") {
